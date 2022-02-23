90ef6b007b5ce9a077705d61e51a2114f94d0edc
myhency <hency.yeo@gmail.com>
2022-02-23 21:17:54 +0900

    함수 위치 변경

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const plays = require('./plays.json');
 const invoices = require('./invoices.json');
 
 function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency", currency: "USD",
+    minimumFractionDigits: 2
+  }).format;
+
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+
+    // 청구 내역을 출력한다.
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += amountFor(perf);
+  }
+  result += `총액: ${format(totalAmount/100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -37,27 +58,6 @@ function statement(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
-
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency", currency: "USD",
-    minimumFractionDigits: 2
-  }).format;
-
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
-    // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
-      perf.audience
-    }석)\n`;
-    totalAmount += amountFor(perf);
-  }
-  result += `총액: ${format(totalAmount/100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
 }
 
 try {
