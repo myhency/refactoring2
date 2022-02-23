f09f6ba9e07fd3d48b7f9bebe2d256066fdb32d2
myhency <hency.yeo@gmail.com>
2022-02-23 21:15:20 +0900

    적립 포인트 계산 코드 추출하기

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ function statement(invoice, plays) {
     return result;
   }
 
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -39,12 +47,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
