116664ad0682c27f4d105bf094057b8284a41e63
myhency <hency.yeo@gmail.com>
2022-02-23 21:52:04 +0900

    함수 추출하기

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const plays = require('./plays.json');
 const invoices = require('./invoices.json');
 
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  
+
   for (let perf of invoice.performances) {
-    
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    
   }
-  
+
   result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
@@ -58,7 +60,7 @@ function statement(invoice, plays) {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber / 100); 
+    }).format(aNumber / 100);
   }
 
   function totalVolumeCredits() {
