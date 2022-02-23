6d93fd9435ef1ffe051ca74ba3b384165f6d05e8
myhency <hency.yeo@gmail.com>
2022-02-23 23:50:41 +0900

    중간 데이터를 사용하도록 변경

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function renderPlainText(data, plays) {
 
   for (let perf of data.performances) {
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
   }
@@ -32,14 +32,10 @@ function renderPlainText(data, plays) {
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   function amountFor(aPerformance) {
     let result = 0;
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy": // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -54,7 +50,7 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
 
     return result;
@@ -63,7 +59,7 @@ function renderPlainText(data, plays) {
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
