09f8e647922ac5202627b661573bee18928ef4a2
myhency <hency.yeo@gmail.com>
2022-02-23 21:04:26 +0900

    임시 변수를 질의 함수로 바꾸기

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const plays = require('./plays.json');
 const invoices = require('./invoices.json');
 
-function amountFor(aPerformance, play) {
-  let result = 0;
-
-  switch (play.type) {
-    case "tragedy": // 비극
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy": // 희극
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
+function statement(invoice, plays) {
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 
-  return result;
+  function amountFor(aPerformance, play) {
+    let result = 0;
 
-}
+    switch (play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
 
-function statement(invoice, plays) {
+    return result;
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -36,7 +39,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // 포인트를 적립한다.
