eb83a2ef3a75a7645f70c16cad261c2ff7d651cc
myhency <hency.yeo@gmail.com>
2022-02-23 21:07:54 +0900

    변수 인라인하기

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
 
     switch (playFor(aPerformance).type) {
