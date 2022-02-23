72fc7fd0667458bbe717a6c4e1ea679b17146410
myhency <hency.yeo@gmail.com>
2022-02-23 21:11:14 +0900

    변수 인라인하기

--- index.js
+++ index.js
@@ -39,8 +39,6 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    // const play = playFor(perf);
-    let thisAmount = amountFor(perf, playFor(perf));
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -49,10 +47,10 @@ function statement(invoice, plays) {
       volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
   result += `총액: ${format(totalAmount/100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
