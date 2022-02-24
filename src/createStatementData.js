2a71ec4ddad3f9b6667abd39682d013a26e0d14e
myhency <hency.yeo@gmail.com>
2022-02-24 15:20:58 +0900

    함수들을 계산기로 옮기기

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -1,64 +1,44 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = createStatementData;
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
-var PerformanceCalculator = /*#__PURE__*/function () {
-  function PerformanceCalculator(aPerformance) {
-    _classCallCheck(this, PerformanceCalculator);
-
+class PerformanceCalculator {
+  constructor(aPerformance, ) {
     this.performance = aPerformance;
     this.play = aPlay;
   }
 
-  _createClass(PerformanceCalculator, [{
-    key: "amount",
-    get: function get() {
-      var result = 0;
+  get amount() {
+    let result = 0;
 
-      switch (this.play.type) {
-        case "tragedy":
-          // 비극
-          result = 40000;
-
-          if (this.performance.audience > 30) {
-            result += 1000 * (this.performance.audience - 30);
-          }
-
-          break;
-
-        case "comedy":
-          // 희극
-          result = 30000;
-
-          if (this.performance.audience > 20) {
-            result += 10000 + 500 * (this.performance.audience - 20);
-          }
-
-          result += 300 * this.performance.audience;
-          break;
-
-        default:
-          throw new Error("\uC54C \uC218 \uC5C6\uB294 \uC7A5\uB974: ".concat(this.play.type));
-      }
-
-      return result;
+    switch (this.play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
     }
-  }]);
 
-  return PerformanceCalculator;
-}();
+    return result;
+  }
 
-function createStatementData(invoice, plays) {
-  var result = {};
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
+}
+
+export default function createStatementData(invoice, plays) {
+  const result = {};
   result.customer = invoice.customer;
   result.performances = invoice.performances.map(enrichPerformance);
   result.totalAmount = totalAmount(result);
@@ -66,24 +46,19 @@ function createStatementData(invoice, plays) {
   return result;
 
   function totalVolumeCredits(data) {
-    return data.performances.reduce(function (total, p) {
-      return total + p.amount;
-    }, 0);
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalAmount(data) {
-    return data.performances.reduce(function (total, p) {
-      return total + p.volumeCredits;
-    }, 0);
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function enrichPerformance(aPerformance) {
-    var calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
-    var result = Object.assign({}, aPerformance); // 얕은 복사 수행
-
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const result = Object.assign({}, aPerformance); // 얕은 복사 수행
     result.play = calculator.play;
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
@@ -91,10 +66,15 @@ function createStatementData(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
+  function amountFor(aPerformance) {
+    return PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
+  }
+
   function volumeCreditsFor(aPerformance) {
-    var result = 0;
+    let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    if ("comedy" === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 }
--- src/statement.js
+++ src/statement.js
@@ -1,57 +1,31 @@
-"use strict";
+import fs from 'fs';
+import util from 'util';
+import createStatementData from './createStatementData.js';
 
-var _fs = _interopRequireDefault(require("fs"));
-
-var _util = _interopRequireDefault(require("util"));
-
-var _createStatementData = _interopRequireDefault(require("./createStatementData.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-var playsFile = _fs["default"].readFileSync('./plays.json', 'utf-8');
-
-var invoicesFile = _fs["default"].readFileSync('./invoices.json', 'utf-8');
-
+var playsFile = fs.readFileSync('./plays.json', 'utf-8');
+var invoicesFile = fs.readFileSync('./invoices.json', 'utf-8');
 var plays = JSON.parse(playsFile);
 var invoices = JSON.parse(invoicesFile);
 
 function statement(invoice, plays) {
-  return renderPlainText((0, _createStatementData["default"])(invoice, plays));
+  return renderPlainText(createStatementData(invoice, plays));
 }
 
 function htmlStatement(invoice, plays) {
-  return renderHtml((0, _createStatementData["default"])(invoice, plays));
+  return renderHtml(createStatementData(invoice, plays));
 }
 
 function renderHtml(data) {
-  var result = "<h1>\uCCAD\uAD6C \uB0B4\uC5ED (\uACE0\uAC1D\uBA85: ".concat(data.customer, ")</h1>\n");
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
   result += "<table>\n";
   result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
-
-  var _iterator = _createForOfIteratorHelper(data.performances),
-      _step;
-
-  try {
-    for (_iterator.s(); !(_step = _iterator.n()).done;) {
-      var perf = _step.value;
-      result += "  <tr><td>".concat(perf.play.name, "</td><td>(").concat(perf.audience, "\uC11D)</td>");
-      result += "<td>".concat(usd(perf.amount), "</td></tr>\n");
-    }
-  } catch (err) {
-    _iterator.e(err);
-  } finally {
-    _iterator.f();
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
-
   result += "</table>\n";
-  result += "<p>\uCD1D\uC561: <em>".concat(usd(data.totalAmount), "</em></p>\n");
-  result += "<p>\uC801\uB9BD \uD3EC\uC778\uD2B8: <em>".concat(data.totalVolumeCredits, "</em>\uC810</p>\n");
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
   return result;
 }
 
@@ -59,38 +33,31 @@ function usd(aNumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-    minimumFractionDigits: 2
+    minimumFractionDigits: 2,
   }).format(aNumber / 100);
 }
 
 function renderPlainText(data, plays) {
-  var result = "\uCCAD\uAD6C \uB0B4\uC5ED (\uACE0\uAC1D\uBA85: ".concat(data.customer, ")\n");
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-  var _iterator2 = _createForOfIteratorHelper(data.performances),
-      _step2;
-
-  try {
-    for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
-      var perf = _step2.value;
-      // 청구 내역을 출력한다.
-      result += " ".concat(perf.play.name, ": ").concat(usd(perf.amount), " (").concat(perf.audience, "\uC11D)\n");
-    }
-  } catch (err) {
-    _iterator2.e(err);
-  } finally {
-    _iterator2.f();
+  for (let perf of data.performances) {
+    // 청구 내역을 출력한다.
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    }석)\n`;
   }
 
-  result += "\uCD1D\uC561: ".concat(usd(data.totalAmount), "\n");
-  result += "\uC801\uB9BD \uD3EC\uC778\uD2B8: ".concat(data.totalVolumeCredits, "\uC810\n");
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
 }
 
 try {
-  var plainTextResult = statement(invoices, plays);
+  const plainTextResult = statement(invoices, plays);
   console.log(plainTextResult);
-  var htmlTextResult = htmlStatement(invoices, plays);
-  console.log(_util["default"].inspect(htmlTextResult, false, null, true));
+  const htmlTextResult = htmlStatement(invoices, plays);
+  console.log(util.inspect(htmlTextResult, false, null, true));
 } catch (error) {
-  console.log(error);
+  console.log(error)
 }
+
